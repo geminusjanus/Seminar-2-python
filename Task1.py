efbf66ca8bca7f7228d343733dca10afd41b23c5
Nikita <silvernikchuprov@gmail.com>
2022-12-20 14:14:33 +0300

    Task1 done

---
+++ Task1.py
@@ -0,0 +1,27 @@
+# Напишите программу, которая принимает на вход вещественное число и показывает сумму его цифр.
+
+# Пример:
+
+# - 6782 -> 23
+# - 0,56 -> 11
+
+num = float(input("Enter floating point number: "))
+
+def sumDigits(num):
+    sum = 0
+    for i in str(num):
+        if i != ".":
+            sum += int(i)
+    return sum
+
+print(f"Сумма цифр = {sumDigits(num)}")
+
+# def isfloat(num):
+#     try:
+#         float(num)
+#         return True
+#     except ValueError:
+#         return False
+
+# print(isfloat('s12'))
+# print(isfloat('1.123'))
--- Task2.py
+++ Task2.py
@@ -1,8 +1,8 @@
-# n = int(input('Enter number N '))
-# multiplication = 0
-# for i in range(n):
-#     multiplication = i * n
-#     print(multiplication)
+# Напишите программу, которая принимает на вход число N и выдает набор произведений чисел от 1 до N.
+
+# *Пример:*
+
+# - пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
 
 n = int(input('Enter number N '))
 multiplication = 1
